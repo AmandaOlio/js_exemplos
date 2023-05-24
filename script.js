e9c783f52c49fe26e5da65b3c732e6cc7185dd3b
Amandinha <amandaaa.b.oliveira@gmail.com>
2023-05-24 14:53:21 -0300

    commit update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var email = prompt("Digite seu e-mail");
-alert("O e-mail " + email + "foi coletado com sucesso");
+// var email = prompt("Digite seu e-mail");
+// alert("O e-mail " + email + "foi coletado com sucesso");
 
 
 // var valor = confirm("Vamos juntos aprender a programar em  JS?");
@@ -8,3 +8,22 @@ alert("O e-mail " + email + "foi coletado com sucesso");
 // } else {
 //     alert("Opção errada, aperte F5 para recarregar a página.");
 // }
+
+
+//var email = prompt("Digite seu e-mail");
+//if (email == true) {
+   // alert("O e-mail " + email + " foi coletado com sucesso ")
+//} else {
+    //alert("não foi coletado")
+//}
+
+
+
+var prova = parseFloat(prompt("Qual a nota da prova?"));
+var trabalho = parseFloat(prompt("Qual a nota do trabalho?"));
+
+var notaFinal = prova + trabalho; 
+
+alert(notaFinal);
+
+
